f15840603946260019ec856024bff0d3c5f865fd
iananevich <ivanshahter71@gmail.com>
2023-11-14 17:14:39 +0300

    add lesson 26

--- rest/serializers.py
+++ rest/serializers.py
@@ -1,29 +1,77 @@
 from rest_framework import serializers
 
-from rest.models import Book
+from rest.models import Book, Author
 
 
-class AuthorListSerializer(serializers.Serializer):
-    first_name = serializers.CharField()
-    last_name = serializers.CharField()
-    email = serializers.EmailField()
+# class BookSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only=True)
+#     name = serializers.CharField()
+#     description = serializers.CharField()
+#     year = serializers.IntegerField()
+#     author_id = serializers.IntegerField()
+#
+#     def create(self, validated_data):
+#         return Book.objects.create(**validated_data)
+    #
+    # def update(self, instance, validated_data):
+    #     instance.name = validated_data.get('name', instance.name)
+    #     instance.description = validated_data.get('description', instance.description)
+    #     instance.year = validated_data.get('year', instance.year)
+    #     instance.author_id = validated_data.get('author_id', instance.author_id)
+    #
+    #     instance.save()
+    #     return instance
 
 
-class BookSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField()
-    description = serializers.CharField()
-    year = serializers.IntegerField()
-    author_id = serializers.IntegerField()
+class AuthorsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = '__all__'
 
-    def create(self, validated_data):
-        return Book.objects.create(**validated_data)
 
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.description = validated_data.get('description', instance.description)
-        instance.year = validated_data.get('year', instance.year)
-        instance.author_id = validated_data.get('author_id', instance.author_id)
+class BookListSerializer(serializers.ModelSerializer):
+    author_name = serializers.SerializerMethodField()
 
-        instance.save()
-        return instance
+    class Meta:
+        model = Book
+        fields = ('id', 'name', 'year', 'author_name')
+        read_only_fields = ('author_name', )
+
+    def get_author_name(self, obj):
+        return obj.author.full_name
+
+
+class BookCreateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Book
+        fields = ('id', 'name', 'year', 'description', 'author')
+
+
+class BookUpdateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Book
+        fields = ('id', 'name', 'year', 'description', 'author')
+
+
+class BookRetrieveSerializer(serializers.ModelSerializer):
+    author = AuthorsSerializer()
+
+    class Meta:
+        model = Book
+        fields = ('id', 'name', 'year', 'description', 'author')
+        extra_kwargs = {
+            'author': {'write_only': True},
+        }
+
+
+class BookRecentBooksSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Book
+        fields = ('id', 'name')
+
+
+
+
--- rest/urls.py
+++ rest/urls.py
@@ -1,10 +1,23 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 
 from rest import views
 
 app_name = 'books'
 
-urlpatterns = [
-    path('books/', views.BookListCreateView.as_view(), name='books'),
-    path('books/<int:pk>', views.BookRetrieveUpdateDeleteView.as_view(), name='books-update'),
-]
+# urlpatterns = [
+#     path('books/', views.BookListCreateView.as_view(), name='books'),
+#     path('books/<int:pk>', views.BookRetrieveUpdateDeleteView.as_view(), name='books-update'),
+# path('books/', views.BookView.as_view(), name='books'),
+# path('books/<int:pk>', views.SingleBookView.as_view(), name='book'),
+# path('books/', views.BookViewSet.as_view({'get': 'list'}), name='books'),
+# path('books/<int:pk>', views.BookViewSet.as_view({'get': 'retrieve'}), name='book'),
+# ]
+
+router = DefaultRouter()
+router.register(r'books', views.BookViewSet, basename='books')
+router.register(r'authors', views.AuthorViewSet, basename='authors')
+
+
+# urlpatterns += router.urls
+urlpatterns = router.urls
